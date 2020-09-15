52d1041dba2485edd9bc015bb2919045007b9c65
Elisandro Moreira <elisandro.moreira@univille.br>
2020-09-15 01:08:30 -0300

    Add styles to Main Component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>NextJS Boilerplate</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Tittle>NextJS Boilerplate</S.Tittle>
+    <S.Description>
+      Typescript, React JS, Next JS e Styles Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 );
 
 export default Main;
